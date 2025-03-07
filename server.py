25fce012ac79dc56d78d8a88cc5969e5b7ab4631
Seyed Moied Seyedi <moied.seyedi.85@gmail.com>
2025-03-07 20:31:15 +0330

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask , render_template
 from flask_socketio import SocketIO , emit
 
-app = Flask("application")
-socket = SocketIO(app)
+app = Flask("application",template_folder="",static_folder="")
+socket = SocketIO(app,async_mode="eventlet")
 
 @app.route("/")
 def home():
-    return "This is the server side !!"
+    return render_template("index.html")
 
 @socket.on("connect")
 def cl_con():
