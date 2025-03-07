c82af79ee6083840f82baa280ee1e15a83e64114
Seyed Moied Seyedi <moied.seyedi.85@gmail.com>
2025-03-07 20:57:55 +0330

    Add files via upload

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import eventlet
 eventlet.monkey_patch()
 
 from flask import Flask , render_template
-from flask_socketio import SocketIO , emit
+from flask_socketio import SocketIO , emit , join_room , leave_room
 
 app = Flask("application",template_folder="",static_folder="")
 socket = SocketIO(app,async_mode="eventlet")
@@ -18,10 +18,18 @@ def cl_con():
 @socket.on("disconnect")
 def cl_dis():
     print("A client disconnected !!")
+
+@socket.on("join")
+def join_to(data):
+    join_room(data["room"])
+
+@socket.on("leave")
+def leave_to(data):
+    leave_room(data["room"])
     
 @socket.on("message")
 def message(data):
-    emit("message",data)
+    emit("message",data["message"],to=data["room"])
 
 if __name__ == "__main__":
     socket.run(app)
